bfbbea0bcfc593f9cfa4c704af322dd03e2d1148
nitishapal <palnitishaece26@gmail.com>
2021-01-19 10:03:43 +0530

    Add all my files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { HttpService } from './services/http.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+  map: google.maps.Map;
   title = 'angularsssignment';
+  states: any = [];
+  cities: any = [];
+  lat = 40.730610;
+  lng = -73.935242;
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8,
+  };
+  marker: google.maps.Marker;
+
+
+  constructor(private httpservice: HttpService) { }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.httpservice.getData().subscribe((res: any) => {
+      this.states = res.states;
+      console.log(res.states);
+    });
+
+  }
+  selectDistrict(cities) {
+    debugger;
+    this.cities = cities.districts;
+    console.log(cities.districts);
+  }
+  selectCity(city){
+console.log(city);
+  }
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement,
+    this.mapOptions);
+    this.marker = new google.maps.Marker({
+      position: this.coordinates,
+      map: this.map,
+    });
+    this.marker.setMap(this.map);
+   }
+   ngAfterViewInit() {
+    this.mapInitializer();
+  }
+
 }
